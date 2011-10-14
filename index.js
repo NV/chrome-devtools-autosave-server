9a865cac7a674667984cccd2f8e7f57280056a4e
Nikita Vasilyev <me@elv1s.ru>
2011-10-14 21:41:24 +0400

    Convert tabs to 4 spaces.

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var fs = require('fs');
 
 http.createServer(function(request, response) {
 
-	response.writeHead(200);
-	response.end('\n');
+    response.writeHead(200);
+    response.end('\n');
 
-	var url = request.headers['x-url'];
+    var url = request.headers['x-url'];
 
-	if (!url || url.indexOf('file://') != 0) {
-		return;
-	}
+    if (!url || url.indexOf('file://') != 0) {
+        return;
+    }
 
-	if (request.headers['x-type'] == 'document') {
-		// I don't yet have a general solution to save new added CSS rules.
-		return;
-	}
+    if (request.headers['x-type'] == 'document') {
+        // I don't yet have a general solution to save new added CSS rules.
+        return;
+    }
 
-	var path = decodeURIComponent(url.replace(/^file:\/\/(?:localhost)?/, ''));
-	request.on('data', function(data) {
-		console.log('Saved a ' + request.headers['x-type'] + ' to ' + path);
-		fs.writeFile(path, data);
-	});
+    var path = decodeURIComponent(url.replace(/^file:\/\/(?:localhost)?/, ''));
+    request.on('data', function(data) {
+        console.log('Saved a ' + request.headers['x-type'] + ' to ' + path);
+        fs.writeFile(path, data);
+    });
 
 }).listen(9104, '127.0.0.1');
 
