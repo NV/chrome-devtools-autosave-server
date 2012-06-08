85e09f2dd0b95e85b03f2711651ae2419cd32098
Nikita Vasilyev <me@elv1s.ru>
2012-06-08 15:28:19 +0400

    Don't yet run autosave on OS launch #9

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-if [ -d $HOME/Library/LaunchAgents/ ]; then
-    # Mac OS X
-    launch_agents_dir=$HOME/Library/LaunchAgents/
-    name='com.chrome.devtools.autosave.launchd'
-    filename=$name.plist
-    cp -v `dirname $0`/$filename $launch_agents_dir
-    launchctl remove $name >/dev/null 2>/dev/null
-    launchctl load $launch_agents_dir/$filename
-elif [ -d /etc/init ]; then
-    # Ubuntu
-    cp -v `dirname $0`/autosave.conf /etc/init/
-    start autosave
+if [ -d $HOME/Library/LaunchAgents/ -o -d /etc/init ]; then
+    echo 'To start autosave on OS launch run:
+    npm run-script -g autosave autostart
+'
 fi
