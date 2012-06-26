02beca647a80bbea2f63ac6561591aa93b9d8c66
Nikita Vasilyev <me@elv1s.ru>
2012-06-27 02:56:29 +0400

    Fix: Autostart script doesn't escape $HOME variable spaces
    Closes #21

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ -d $HOME/Library/LaunchAgents/ ]; then
+if [ -d "$HOME/Library/LaunchAgents/" ]; then
     # Mac OS X
-    launch_agents_dir=$HOME/Library/LaunchAgents/
+    launch_agents_dir="$HOME/Library/LaunchAgents/"
     name='com.chrome.devtools.autosave.launchd'
     filename=$name.plist
-    cp -v `dirname $0`/$filename $launch_agents_dir
+    cp -v `dirname $0`/$filename "$launch_agents_dir"
     launchctl remove $name >/dev/null 2>/dev/null
-    launchctl load $launch_agents_dir/$filename
+    launchctl load "$launch_agents_dir/$filename"
 elif [ -d /etc/init ]; then
     # Ubuntu
     cp -v `dirname $0`/autosave.conf /etc/init/
